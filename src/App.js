431b08713a6e8dbed38bb954c1fecee5509fe608
muhammad wasi81 <wasiarain81@gmail.com>
2021-03-29 01:42:22 +0500

    Created a react random generated advice app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
+import React from "react";
+import axios from "axios";
 
-import './App.css';
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>wasi</h1>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    advice: "",
+  };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{this.state.advice}</h1>
+          <button className="button" onClick={this.fetchAdvice}>
+            <span>GIVE ME ADVICE!</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
